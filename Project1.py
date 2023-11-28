fb0050742030f0f6cca9f9d9e372eb0f2416b02b
Rebecca Feeley <G00425652@atu.ie>
2023-11-28 11:51:07 +0000

    updated probabilties

--- Project1.py
+++ Project1.py
@@ -17,12 +17,13 @@ import matplotlib.pyplot as plt
 ''''
 Probabiltiy of reoffending: 
 
-1. Age - Numerical continuous data 
+1. Age - categorical discrete 
 2. Gender - Categorical nominal data (Or region)
 3. Type of offence - categorical nominal 
 4. Length of imprisonment - 
 5. Education level/economic of criminal - categorical ordinal
 '''
+# Look at those sentenced in 2022 and their probability of reoffending
 
 # import pandas with shortcut 'pd' 
 import pandas as pd 
@@ -31,19 +32,14 @@ import pandas as pd
 # Set the seed so that the numbers can be reproduced.
 np.random.seed(0)  
 
-
-#df = pd.DataFrame('gender': np.random.choice(gender, 1000,p=[0.891, 0.109]))
-
 # 2747 people released in 2020 (41% reoffended within 1 year of release from custodial sentence)
 
-genders = ['Male', 'Female'] 
-result = np.random.choice(genders, 4122, p=[0.96, 0.04]) # setting the probability per https://www.iprt.ie/latest-news/irish-prison-service-annual-report-2022/
+#genders = ['Male', 'Female'] 
+#result = np.random.choice(genders, 4122, p=[0.96, 0.04]) # setting the probability per https://www.iprt.ie/latest-news/irish-prison-service-annual-report-2022/
 # as per 2022 prison population 
-print(result) # displaying the resultant array 
+#print(result) # displaying the resultant array 
 
-ages = 
-
-age = np.random.randint(15,100, 4122)
+#Bernoulli distribution
 
 
 ### 
@@ -53,3 +49,25 @@ age = np.random.randint(15,100, 4122)
 #result = np.random.choice(genders, 5801, p=[0.891, 0.109]) # per https://www.iprt.ie/latest-news/irish-prison-service-annual-report-2022/
 #print(result)
 
+Ages = ['18-24', '25-34', '35-44', '45-54', '55+']
+age = np.random.choice(Ages, 5045, p=[0.153, 0.38, 0.297, 0.118, 0.052])
+#print(age)
+# excludes those not sentenced eg those commited on contempt of court & on remand awaiting trial
+# https://www.irishprisons.ie/wp-content/uploads/documents_pdf/SENTENCED-COMMITTALS-by-Age-and-Gender-Year-2007-to-2022.pdf
+
+#Ages = ['Under 21', '21-25', '26-30', '31-35', '36-40', '41-50' '50+']
+#age = np.random.choice(Ages, 2604, p=[0.057, 0.193, 0.211, 0.184, 0.133, 0.141, 0.078])
+
+
+
+sea.countplot(x=age, order=Ages)
+plt.title('Distribution of Age Groups')
+plt.show()
+
+## If you want to visualize this distribution, you can use a bar plot or a pie chart, which are suitable for categorical data. If you want to fit a known distribution to your data, that’s a more complex task and may not be possible without additional assumptions or data.
+# if you have reason to believe that the underlying age distribution is normal (or any other known distribution), you could fit a model to your data using maximum likelihood estimation or other methods. However, this would require you to assign a numerical value to each age group, which may not accurately reflect the true age distribution.
+#In most cases, the best way to model a distribution across categorical data is to use the observed frequencies (or probabilities) of each category, as you’ve done. This gives you a direct representation of the distribution without making additional assumptions.
+#^^^ # rewrite
+
+
+#total prison releases in 2017 = 2604 
