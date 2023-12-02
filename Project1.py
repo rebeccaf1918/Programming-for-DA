f5570f1ce21ac24d2bcf6672af4c113ad6f45bb4
Rebecca Feeley <G00425652@atu.ie>
2023-12-02 01:49:57 +0000

    updating 4th variable and adding pie chart

--- Project1.py
+++ Project1.py
@@ -27,9 +27,28 @@ np.random.seed(0)
 #In most cases, the best way to model a distribution across categorical data is to use the observed frequencies (or probabilities) of each category, as you’ve done. This gives you a direct representation of the distribution without making additional assumptions.
 #^^^ # rewrite
 
-
 #statuses = np.random.choice([0, 1, 2], n_prisoners, p=[0.597, 0.095, 0.196])
 #education_level = np.random.choice([0, 1, 2, 3, 4])
 #assigning a number to the education level of the prisoner 
 
+Genders = ['Male', 'Female'] 
+result = np.random.choice(Genders, 2604, p=[0.925, 0.075])  # BINOMIAL DISTRIBUTION
+#print(result)
+#another method would be to assign Binomial distribution with 10 trials and probability 0.5 each trial. 
+#fig = np.random.binomial(10, 0.5, 10000)
 
+sea.countplot(x=result, order=Genders)
+plt.title('Distribution of Gender') 
+plt.show()
+
+
+
+p = 2604
+Genders = ['Male', 'Female'] 
+listGender = np.random.choice(Genders, p, p=[0.925, 0.075])
+ 
+Ages = ['Under 21', '21-25', '26-30', '31-35', '36-40', '41-50', '50+']
+listAge = np.random.choice(Ages, 2604, p, p=[0.058, 0.194, 0.211, 0.184, 0.133, 0.141, 0.079])
+d = {'Gender': listGender, 'Age': listAge}
+df = pd.DataFrame(data=d)
+print(df)
